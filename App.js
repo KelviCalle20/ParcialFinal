057f39a3b79ff41fdf86f190d9543eb47ef32e18
KelviCalle20 <callekelvin5@gamil.com>
2025-12-02 08:58:13 -0400

    feat: peticion actializado POST

--- App.js
+++ App.js
@@ -6,41 +6,42 @@ app.use(express.json());
 const PORT = 3000
 
 app.get("/estado", (req, res) => {
-  const datos = new Date();
+    const datos = new Date();
 
-  res.status(200).json({
-    servicio: "backend",
-    version: "1.11.2",
-    entorno: "production",
-    estado: "servicio en funcionamiento",
-    fecha: datos.toISOString().slice(0, 19).replace("T", " "),
-    hora: Date.now()
-  });
+    res.status(200).json({
+        servicio: "backend",
+        version: "1.11.2",
+        entorno: "production",
+        estado: "servicio en funcionamiento",
+        fecha: datos.toISOString().slice(0, 19).replace("T", " "),
+        hora: Date.now()
+    });
 });
 
 app.post("/datos", (req, res) => {
-  const { nombre, correo, edad } = req.body;
+    const { nombre, correo, edad } = req.body;
 
-  if (!nombre || !correo || !edad) {
-    return res.status(400).json({
-      ok: false,
-      mensaje: "Todos los campos son obligatorios"
+    if (!nombre || !correo || !edad) {
+        return res.status(400).json({
+            mensaje: "Todos los campos son obligatorios"
+        });
+    }
+
+    const ahora = new Date();
+
+    const data = {
+        id: Date.now(),
+        nombre,
+        correo,
+        edad
+    };
+
+    res.status(201).json({
+        mensaje: "Registro creado correctamente",
+        data: data,
+        fechaRegistro: ahora.toISOString(),
+        horaRegistro: String(Date.now())
     });
-  }
-
-  const nuevoRegistro = {
-    id: Date.now(),
-    nombre,
-    correo,
-    edad,
-    fechaRegistro: new Date().toISOString()
-  };
-
-  res.status(201).json({
-    ok: true,
-    mensaje: "Registro creado correctamente",
-    data: nuevoRegistro
-  });
 });
 
 app.listen(PORT, () => {
