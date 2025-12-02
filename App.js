eaca9835ae7650da064137e80b53151028d170e6
KelviCalle20 <callekelvin5@gamil.com>
2025-12-02 09:01:58 -0400

    feat: peticion PUT

--- App.js
+++ App.js
@@ -44,6 +44,30 @@ app.post("/datos", (req, res) => {
     });
 });
 
+app.put("/datos/:id", (req, res) => {
+  const { id } = req.params;
+  const { nombre, correo } = req.body;
+
+  if (!nombre || !correo) {
+    return res.status(400).json({
+      mensaje: "Nombre y correo son obligatorios"
+    });
+  }
+
+  const ahora = new Date();
+
+  res.status(200).json({
+    mensaje: "Registro creado correctamente",
+    idUsuario: id,
+    dataactualizados: {
+      nombre: nombre,
+      correo: correo
+    },
+    fechaRegistro: ahora.toISOString(),
+    horaRegistro: String(Date.now())
+  });
+});
+
 app.listen(PORT, () => {
     console.log(`servicio corriendo en http://localhost:${PORT}`);
 })
